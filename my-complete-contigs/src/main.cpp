e83de8ea53b0bd98cbfc292ed7d8b1bb26e9789d
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-05-15 18:41:35 +0300

    ..

--- my-complete-contigs/src/main.cpp
+++ my-complete-contigs/src/main.cpp
@@ -90,7 +90,7 @@ vector<contig> coderodde_project_algorithm(const StaticDigraph& graph,
 		subdivided_graph.erase(removed_arc);
 		
 		// Here, compute the strongly connected components.
-		ListDigraph::NodeMap<int> scc_map;
+		ListDigraph::NodeMap<int> scc_map(subdivided_graph);
 		int number_of_strongly_connected_components =
 				stronglyConnectedComponents(subdivided_graph, scc_map);
 				
