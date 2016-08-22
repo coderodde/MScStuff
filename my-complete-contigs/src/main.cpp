7494fe84db1ce5b48aca661bf61f6c61e664f0de
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-08-22 17:58:57 +0300

    Testing the a-matrix.

--- my-complete-contigs/src/main.cpp
+++ my-complete-contigs/src/main.cpp
@@ -2010,6 +2010,18 @@ static void test_a_matrix_algo()
 		int isz2x2 = static_graph.id(sz2x2); //graph_arcs_to_static_graph_arcs[z2x2];
 		int isz2y2 = static_graph.id(sz2y2); //graph_arcs_to_static_graph_arcs[z2y2];
 		
+		cout << "WHAT:\n";
+		
+		for (const auto& n : u)
+		{
+			for (const auto& nn : u.second)
+			{
+				cout << nn.second << " ";	
+			}
+			
+			cout << "\n";
+		}
+		
 		cout << "phase\n";
 		if (a_matrix[isx1y1][isx2y2] || a_matrix[isx2y2][isx1y1]) {
 			cout << "Some of them!\n";
