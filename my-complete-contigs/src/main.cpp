f029ba4ac3b090dd0eed90766083370010eda7c7
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-08-24 17:28:53 +0300

    Working on m^2.

--- my-complete-contigs/src/main.cpp
+++ my-complete-contigs/src/main.cpp
@@ -1970,6 +1970,8 @@ static void test_a_matrix_algo()
 	cout << "(b,x) " << a_matrix[isb][isa] << "     " << a_matrix[isb][isb] << "     " << a_matrix[isb][isc] << "     " << a_matrix[isb][isd] << "\n";
 	cout << "(c,x) " << a_matrix[isc][isa] << "     " << a_matrix[isc][isb] << "     " << a_matrix[isc][isc] << "     " << a_matrix[isc][isd] << "\n";
 	cout << "(d,x) " << a_matrix[isd][isa] << "     " << a_matrix[isd][isb] << "     " << a_matrix[isd][isc] << "     " << a_matrix[isd][isd] << "\n";
+	
+	cout << "HELLO: " << a_matrix[isa].size() << "\n";
 
 	{
 		ListDigraph list_graph;
