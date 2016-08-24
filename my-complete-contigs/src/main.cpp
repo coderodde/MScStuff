970bd434ae933806762c3d47aafbc7ef18b2fd07
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-08-24 18:36:21 +0300

    Working on m^2.

--- my-complete-contigs/src/main.cpp
+++ my-complete-contigs/src/main.cpp
@@ -231,7 +231,7 @@ static unordered_map<int, unordered_map<int, bool>> compute_a_matrix(const Stati
 		ListDigraph::Arc removed_arc = map_static_digraph_arcs_to_list_digraph_arcs[arc];
 		ListDigraph::Node removed_arc_tail = work_graph.source(removed_arc); // x_1
 		ListDigraph::Node removed_arc_head = work_graph.target(removed_arc); // x_2
-		//int removed_arc_id = work_graph.id(removed_arc);
+		int removed_arc_id = work_graph.id(removed_arc);
 		//work_graph.erase(removed_arc);
 		
 		// Run the DFS in order to find all the nodes reachable from the node x_1:
