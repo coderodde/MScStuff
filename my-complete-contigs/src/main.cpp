60c4e124393c6faf2e33f5b597b75c262d39174d
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-09-20 18:18:23 +0300

    Added a list node to static node ID map.

--- my-complete-contigs/src/main.cpp
+++ my-complete-contigs/src/main.cpp
@@ -2354,8 +2354,8 @@ int main(int argc, char **argv)
 	//test_list_digraph_node_ids();
 	//test_strong_bridges();
 	//test_a_matrix_algo();
-	//test_cycle_reconstruction();
-	//exit(0);
+	test_cycle_reconstruction();
+	exit(0);
 	
 	//////////////////////////////////////////
 	//////////////////////////////////////////
